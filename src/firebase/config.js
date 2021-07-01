8e05b519b42bc455525f88037ac2318b75931729
anasZabili <57200540+anasZabili@users.noreply.github.com>
2021-07-02 01:01:22 +0200

    Fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,7 @@ import "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCAgcdDbWRsgVAowzDNvowskj7b99owFBw",
-  authDomain: "firegram-a0bba.firebaseapp.com",
-  databaseURL: "https://firegram-a0bba.firebaseio.com",
-  projectId: "firegram-a0bba",
-  storageBucket: "firegram-a0bba.appspot.com",
-  messagingSenderId: "389131691187",
-  appId: "1:389131691187:web:9e56991a42304cf82cbcc6",
+  x: "xxx"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
